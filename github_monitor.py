5902babce382bdf5be81d5cb993fe7660be9b359
Tom O'Connell <tom.oconn3ll@gmail.com>
2016-09-18 14:51:56 -0700

    Populating locally hosted RSS feed with events data now.

--- github_monitor.py
+++ github_monitor.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python3
 
-import re
-import socketserver
 from feedgen.feed import FeedGenerator
+import socketserver
+import urllib.request
+import hashlib
+import json
 
 def init_feed():
     fg = FeedGenerator()
@@ -18,6 +20,51 @@ def init_feed():
 
     return fg
 
+def add_general_event(event, feed):
+    """ Adds one GitHub event entry (dict read from JSON) to feed with generic information. """
+
+    fe = feed.add_entry()
+    fe.id(event['created_at'])
+    fe.title(event['type'])
+    desc = event['actor']['login'] + ' ' + event['created_at']
+    fe.description(desc)
+    #fe.enclosure('', 0, '')
+
+def add_comment_event(event, feed):
+    """ Adds richer information about the more interesting events (ones with comments). """
+    
+    fe = feed.add_entry()
+    fe.id(event['created_at'])
+
+    prefix = event['actor']['login'] + ' at ' + event['created_at']
+    fe.title(prefix)
+    desc = prefix + ': ' + event['payload']['comment']['body']
+
+    fe.description(desc)
+    #fe.enclosure('', 0, '')
+
+
+def json2entries(data, feed):
+    """ Takes a dict of urls -> JSON descriptions and adds the feed entries I want. """
+
+    for url in data:
+        for event in data[url]:
+            if not event['type'] == 'IssueCommentEvent':
+                add_general_event(event, feed)
+            else:
+                add_comment_event(event, feed)
+
+
+class MyTCPServer(socketserver.TCPServer):
+    """ To give some state that the stream handler can use. """
+
+    def __init__(self, hostport_tuple, handler, data):
+
+        super().__init__(hostport_tuple, handler)
+        self.feed = init_feed()
+
+        # populates the feed with the github events info
+        json2entries(data, self.feed)
 
 class MyStreamHandler(socketserver.StreamRequestHandler):
     """
@@ -28,8 +75,13 @@ class MyStreamHandler(socketserver.StreamRequestHandler):
     client.
     """
 
+    # TODO can this class get info from its TCPServer?
+
     def handle(self):
-        fg = init_feed()
+        #fg = init_feed()
+        fg = self.server.feed
+
+        #json2entries(
         
         # get the rss feed as a string
         rssfeed = fg.rss_str(pretty=True)
@@ -38,13 +90,31 @@ class MyStreamHandler(socketserver.StreamRequestHandler):
         self.wfile.write(rssfeed)
 
 if __name__ == "__main__":
+    
+    with open('sites.txt', 'r') as f:
+        urls = f.read().strip().split()
+
+    url2hash = dict()
+    hash2url = dict()
+    
+    for url in urls:
+        url2hash[url] = hashlib.md5(url.encode('utf-8')).hexdigest()[:10]
+        hash2url[url2hash[url]] = url
+        urllib.request.urlretrieve(url, './data/' + url2hash[url])
+
+    data = dict()
+
+    for hsh in hash2url:
+        with open('./data/' + hsh, 'r') as fp:
+            data[hash2url[hsh]] = json.load(fp)
+    
     host = True
     
     if host:
-        HOST, PORT = "localhost", 9998
+        HOST, PORT = "localhost", 9997
         
         # Create the server, binding to localhost on port 9999
-        server = socketserver.TCPServer((HOST, PORT), MyStreamHandler)
+        server = MyTCPServer((HOST, PORT), MyStreamHandler, data)
 
         # Activate the server; this will keep running until you
         # interrupt the program with Ctrl-C
