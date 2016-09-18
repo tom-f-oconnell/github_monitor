8f46b944652d6b4190ab66a0951b0a30022eb5e4
Tom O'Connell <tom.oconn3ll@gmail.com>
2016-09-18 12:50:52 -0700

    Added gitignore and broke out feed creation.

--- github_monitor.py
+++ github_monitor.py
@@ -4,6 +4,21 @@ import re
 import socketserver
 from feedgen.feed import FeedGenerator
 
+def init_feed():
+    fg = FeedGenerator()
+
+    fg.id('http://eftm.duckdns.org/rss/')
+    fg.title('Test feed')
+    fg.author({'name': '', 'email': 'yeemail@yup.hom'} )
+    # not sure what 'alternate' does?
+    fg.link(href='http://eftm.duckdns.org', rel='alternate')
+    fg.link( href='http://eftm.duckdns.org/rss', rel='self')
+    fg.language('en')
+    fg.description('Null')
+
+    return fg
+
+
 class MyStreamHandler(socketserver.StreamRequestHandler):
     """
     The request handler class for our server.
@@ -14,47 +29,27 @@ class MyStreamHandler(socketserver.StreamRequestHandler):
     """
 
     def handle(self):
-
-        print('Handling request')
-          
-        # self.request is the TCP socket connected to the client
-        # self.data = self.request.recv(1024).strip()
+        fg = init_feed()
         
-        # self.data = self.rfile.readline.strip()
-        # print("{} wrote:".format(self.client_address[0]))
-        # print(self.data)
-
-        fg = FeedGenerator()
-        # won't print out anything at all. kind of strange
-        dir(fg)
-
-        # ?
-        fg.id('http://eftm.duckdns.org/rss/')
-        fg.title('Test feed')
-        fg.author({'name': '', 'email': 'yeemail@yup.hom'} )
-        # not sure what 'alternate' does?
-        fg.link(href='http://eftm.duckdns.org', rel='alternate')
-        # ?
-        fg.link( href='http://eftm.duckdns.org/rss', rel='self')
-        fg.language('en')
-        fg.description('Null')
-
         # get the rss feed as a string
         rssfeed = fg.rss_str(pretty=True)
-        print(rssfeed)
         
-        # Likewise, self.wfile is a file-like object used to write back
-        # to the client
-        print(dir(self))
+        # self.wfile is a file-like object used to write back to the client
         self.wfile.write(rssfeed)
 
 if __name__ == "__main__":
-    HOST, PORT = "localhost", 9999
+    host = True
+    
+    if host:
+        HOST, PORT = "localhost", 9998
+        
+        # Create the server, binding to localhost on port 9999
+        server = socketserver.TCPServer((HOST, PORT), MyStreamHandler)
 
-    # Create the server, binding to localhost on port 9999
-
-    server = socketserver.TCPServer((HOST, PORT), MyStreamHandler)
-    # Activate the server; this will keep running until you
-    # interrupt the program with Ctrl-C
-
-    server.serve_forever()
+        # Activate the server; this will keep running until you
+        # interrupt the program with Ctrl-C
+        server.serve_forever()
+    else:
+        # Otherwise output to a file, which can be uploaded to hosting separately
+        output = './rss'
+        
